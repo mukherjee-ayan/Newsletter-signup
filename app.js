bab8cabea5ac3db5bd67732f68cfb23e517e6b68
Ayan Mukherjee <ayan13ayan.am@gmail.com>
2022-04-24 00:02:19 +0530

    Changed the listen log message

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get("/", function(req, res){
 });
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("Server is running on port 3000");
+  console.log("Server is running.");
 });
 
 
